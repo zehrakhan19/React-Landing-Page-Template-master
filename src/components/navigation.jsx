2709e747b4f41aaa1421e9f944190e4f820f6fe7
zehrakhan19 <zehraamirkhan19@gmail.com>
2024-04-06 00:19:17 +0530

    fixed navbar bg color

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -3,13 +3,21 @@ import { isMobile } from "react-device-detect";
 
 export const Navigation = (props) => {
   const [navbarBg, setNavbarBg] = useState("transparent");
+  const [navbarLink, setNavbarLink] = useState(isMobile ? "#888281" : "#fff");
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setNavbarBg("white");
+      if (isMobile) {
+        setNavbarBg("#fff");
+        setNavbarLink("#888281");
       } else {
-        setNavbarBg("transparent");
+        if (window.scrollY > 0) {
+          setNavbarBg("#fff");
+          setNavbarLink("#888281");
+        } else {
+          setNavbarBg("#888281");
+          setNavbarLink("#fff");
+        }
       }
     };
 
@@ -55,42 +63,69 @@ export const Navigation = (props) => {
           className={`collapse navbar-collapse ${
             isMobile ? "navbar-collapse-mob" : "navbar-collapse-lap"
           }`}
-          // className="collapse navbar-collapse"
           id="bs-example-navbar-collapse-1"
         >
           <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href="#features" className="page-scroll">
+              <a
+                href="#features"
+                className="page-scroll"
+                style={{ color: navbarLink }}
+              >
                 Features
               </a>
             </li>
             <li>
-              <a href="#about" className="page-scroll">
+              <a
+                href="#about"
+                className="page-scroll"
+                style={{ color: navbarLink }}
+              >
                 About
               </a>
             </li>
             <li>
-              <a href="#services" className="page-scroll">
+              <a
+                href="#services"
+                className="page-scroll"
+                style={{ color: navbarLink }}
+              >
                 Services
               </a>
             </li>
             <li>
-              <a href="#portfolio" className="page-scroll">
+              <a
+                href="#portfolio"
+                className="page-scroll"
+                style={{ color: navbarLink }}
+              >
                 Gallery
               </a>
             </li>
             <li>
-              <a href="#testimonials" className="page-scroll">
+              <a
+                href="#testimonials"
+                className="page-scroll"
+                style={{ color: navbarLink }}
+              >
                 Testimonials
               </a>
             </li>
             <li>
-              <a href="#team" className="page-scroll">
+              <a
+                href="#team"
+                className="page-scroll"
+                style={{ color: navbarLink }}
+              >
                 Team
               </a>
             </li>
             <li>
-              <a href="#contact" className="page-scroll">
+              <a
+                href="#contact"
+                className="page-scroll"
+                style={{ color: navbarLink }}
+              >
                 Contact
               </a>
             </li>
